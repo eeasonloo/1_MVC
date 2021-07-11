5b2e13110b5115d46300746a01566ed70ddfb3f0
eeasonloo <3281038296@qq.com>
2021-07-11 12:23:24 +0800

    Find All Users Info Controller

---
+++ 1_MVCPrototype/src/com/eason/web/servlet/UserListServlet.java
@@ -0,0 +1,32 @@
+package com.eason.web.servlet;
+
+import com.eason.domain.User;
+import com.eason.service.UserService;
+import com.eason.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/userListServlet")
+public class UserListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserService service = new UserServiceImpl();
+        List<User> usersList = service.findAll();
+
+        request.setAttribute("usersList",usersList);
+
+        request.getRequestDispatcher("/list.jsp").forward(request,response);
+
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
