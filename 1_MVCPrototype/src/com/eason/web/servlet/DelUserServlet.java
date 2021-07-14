84e5a628c8f54cda1a99df5bd9c694e74d62e62e
eeasonloo <3281038296@qq.com>
2021-07-14 16:50:21 +0800

    Delete User Button Feature Commit

---
+++ 1_MVCPrototype/src/com/eason/web/servlet/DelUserServlet.java
@@ -0,0 +1,29 @@
+package com.eason.web.servlet;
+
+import com.eason.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/delUserServlet")
+public class DelUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+        String id = request.getParameter("id");
+
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.deleteUser(id);
+
+        response.sendRedirect("/userListServlet");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
