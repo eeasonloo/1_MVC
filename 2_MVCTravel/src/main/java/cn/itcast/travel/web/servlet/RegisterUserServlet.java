6cd99d10a58e82da66a516260545b4554a5170d1
eeasonloo <3281038296@qq.com>
2021-07-20 20:30:24 +0800

    Register New User Servlet First Commit

---
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/web/servlet/RegisterUserServlet.java
@@ -0,0 +1,60 @@
+package cn.itcast.travel.web.servlet;
+
+import cn.itcast.travel.domain.ResultInfo;
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/user/register")
+public class RegisterUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String[]> map = request.getParameterMap();
+
+        /*String[] emails = map.get("email");
+        System.out.println(emails[0]);*/
+
+        User user = new User();
+
+        try {
+            BeanUtils.populate(user,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        UserServiceImpl userService = new UserServiceImpl();
+        boolean flag = userService.register(user);
+
+        ResultInfo resultInfo = new ResultInfo();
+
+        if(flag){
+            resultInfo.setFlag(true);
+        }else{
+            resultInfo.setFlag(false);
+            resultInfo.setErrorMsg("The register doesnt success!");
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(resultInfo);
+
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
