0b1f5ef904effb20c860ef3239a5915c66088903
eeasonloo <3281038296@qq.com>
2021-07-25 17:38:39 +0800

    Refactor Logout Servlet Commit

--- 2_MVCTravel/src/main/java/cn/itcast/travel/web/servlet/LogoutUserServlet.java
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/web/servlet/LogoutUserServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/loggedOutServlet")
-public class LoggedOutServlet extends HttpServlet {
+@WebServlet("/logoutServlet")
+public class LogoutUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getSession().invalidate();
         response.sendRedirect("login.html");
