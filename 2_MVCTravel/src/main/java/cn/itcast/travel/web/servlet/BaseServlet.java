b178db05183568b7b566627f71e635e50f86bfa6
eeasonloo <3281038296@qq.com>
2021-07-26 18:51:41 +0800

    Base Servlet add writeValue and writeValueAsString Functions

--- 2_MVCTravel/src/main/java/cn/itcast/travel/web/servlet/BaseServlet.java
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/web/servlet/BaseServlet.java
@@ -1,13 +1,18 @@
 package cn.itcast.travel.web.servlet;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 
 public class BaseServlet extends HttpServlet {
@@ -17,7 +22,7 @@ public class BaseServlet extends HttpServlet {
         System.out.println(requestURI); ///travel/user/add
 
         String methodName = requestURI.substring(requestURI.lastIndexOf("/") + 1);
-
+        System.out.println(methodName);
 
         try {
             Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
@@ -29,7 +34,21 @@ public class BaseServlet extends HttpServlet {
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
+    }
 
+    public String writeValueAsString(List categoryList) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(categoryList);
+        return json;
+    }
+
+    public void writeValue(Object object, HttpServletResponse response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(response.getOutputStream(),object);
 
     }
+
 }
+
+
