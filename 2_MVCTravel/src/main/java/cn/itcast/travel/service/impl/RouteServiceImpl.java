f0f7ba95c172919d32e944cf8b411e6c34802fd8
eeasonloo <3281038296@qq.com>
2021-07-29 22:46:20 +0800

    RouteServiceImpl First Commit

---
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/service/impl/RouteServiceImpl.java
@@ -0,0 +1,33 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.dao.RouteDao;
+import cn.itcast.travel.dao.impl.RouteDaoImpl;
+import cn.itcast.travel.domain.PageBean;
+import cn.itcast.travel.domain.Route;
+import cn.itcast.travel.service.RouteService;
+import com.sun.xml.internal.fastinfoset.util.ValueArrayResourceException;
+
+import java.util.List;
+
+public class RouteServiceImpl implements RouteService {
+
+    private RouteDao routeDao = new RouteDaoImpl();
+
+    @Override
+    public PageBean<Route> pageQuery(int cid, int currentPage, int pageSize) {
+        PageBean<Route> pageBean = new PageBean<>();
+
+        int totalCount = routeDao.findTotalCount(cid);
+        int totalPage = totalCount % pageSize == 0 ? totalCount/pageSize : totalCount/pageSize+1;
+        int start = (currentPage-1) * pageSize;
+        List<Route> routeList = routeDao.findByPage(cid,pageSize,start);
+
+        pageBean.setTotalCount(totalCount);
+        pageBean.setTotalPage(totalPage);
+        pageBean.setCurrentPage(currentPage);
+        pageBean.setPageSize(pageSize);
+        pageBean.setList(routeList);
+
+        return pageBean;
+    }
+}
