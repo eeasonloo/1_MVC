776ba69e3fc2e368d52c1f8228915aa431cb3c89
eeasonloo <3281038296@qq.com>
2021-07-29 22:47:00 +0800

    RouteDao and RouteDaoImpl First Commit

---
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/dao/impl/RouteDaoImpl.java
@@ -0,0 +1,30 @@
+package cn.itcast.travel.dao.impl;
+
+import cn.itcast.travel.dao.RouteDao;
+import cn.itcast.travel.domain.Route;
+import cn.itcast.travel.util.JDBCUtils;
+import cn.itcast.travel.util.JedisUtil;
+import com.alibaba.druid.util.JdbcUtils;
+import jdk.nashorn.internal.ir.CallNode;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class RouteDaoImpl implements RouteDao {
+
+    JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+
+    @Override
+    public int findTotalCount(int cid) {
+        String sql = "select count(*) from tab_route where cid = ?";
+        return template.queryForObject(sql,Integer.class,cid);
+    }
+
+    @Override
+    public List<Route> findByPage(int cid, int pageSize, int start) {
+
+        String sql = "select * from tab_route where cid = ? limit ?,?";
+        return template.query(sql,new BeanPropertyRowMapper<Route>(Route.class),cid,start,pageSize);
+    }
+}
