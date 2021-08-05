983ac4d95d495323d3f2ac6ca6a97ba7ce59e674
eeasonloo <3281038296@qq.com>
2021-08-05 14:43:49 +0800

    FavoriteDaoImplTest isFavourite Testing Commit

---
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/test/FavouriteDaoImplTest.java
@@ -0,0 +1,17 @@
+package cn.itcast.travel.test;
+
+import cn.itcast.travel.dao.FavouriteDao;
+import cn.itcast.travel.dao.impl.FaouriteDaoImpl;
+import cn.itcast.travel.domain.Favorite;
+import org.junit.Test;
+
+public class FavouriteDaoImplTest {
+    FavouriteDao favouriteDao = new FaouriteDaoImpl();
+
+    @Test
+    public void isFavourite(){
+        Favorite favorite = favouriteDao.isFavourite(1,13);
+        System.out.println(favorite);
+
+    }
+}
