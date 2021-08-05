9c0326c4920eff7f293d1d3b8c8af1f7df907ccc
eeasonloo <3281038296@qq.com>
2021-08-05 14:43:15 +0800

    FavoriteService and Impl isFavourite Checking Commit

---
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/service/FavouriteService.java
@@ -0,0 +1,5 @@
+package cn.itcast.travel.service;
+
+public interface FavouriteService {
+    Boolean isFavourite(int rid, int uid);
+}
---
+++ 2_MVCTravel/src/main/java/cn/itcast/travel/service/impl/FavouriteServiceImpl.java
@@ -0,0 +1,24 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.dao.FavouriteDao;
+import cn.itcast.travel.dao.impl.FaouriteDaoImpl;
+import cn.itcast.travel.domain.Favorite;
+import cn.itcast.travel.service.FavouriteService;
+import cn.itcast.travel.util.JDBCUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class FavouriteServiceImpl implements FavouriteService {
+
+
+    FavouriteDao favouriteDao = new FaouriteDaoImpl();
+
+    @Override
+    public Boolean isFavourite(int rid, int uid) {
+        Favorite favorite = favouriteDao.isFavourite(rid,uid);
+
+        if(favorite == null){
+            return false;
+        }
+        return true;
+    }
+}
